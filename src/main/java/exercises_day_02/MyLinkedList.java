d18db59a1a19f0f8d426095c89baf7a4f0f9d851
Lars Kristiansen <larkr23@student.sdu.dk>
2025-10-15 01:43:37 +0200

    Add delete insert etc to MyLinkedList

--- src/main/java/exercises_day_02/MyLinkedList.java
+++ src/main/java/exercises_day_02/MyLinkedList.java
@@ -1,7 +1,9 @@
 package exercises_day_02;
 
+import com.sun.security.jgss.GSSUtil;
+
 public class MyLinkedList {
-    private final Object element;
+    private Object element;
     private MyLinkedList tail;
 
     public MyLinkedList(Object element) {
@@ -30,6 +32,46 @@ public class MyLinkedList {
         } current.tail = new MyLinkedList(element);
     }
 
+    public void insert(Object element, int idx) {
+        if (idx < 0) return;
+        if (idx == 0) {
+            this.tail = new MyLinkedList(this.element, this.tail);
+            this.element = element;
+            return;
+        }
+        int count = 0;
+        MyLinkedList prev = this;
+        while (prev != null && count < idx - 1) {
+            count++;
+            prev = prev.tail;
+        }
+        prev.tail = new MyLinkedList(element, prev.tail);
+    }
+    public Object delete(int idx) {
+        if (idx < 0) {
+            return null;
+        }
+        if (idx == 0) {
+            Object removed = this.element;
+            if (this.tail != null) {
+                this.element = this.tail.element;
+                this.tail = this.tail.tail;
+            } else {
+                this.element = null;
+            } return removed;
+        } MyLinkedList prev = this;
+        int i = 0;
+        while (prev.tail != null && i < idx - 1) {
+            i++;
+            prev = prev.tail;
+        } if (prev == null || prev.tail == null) {
+            return null;
+        }
+        Object removed = prev.tail.element;
+        prev.tail = prev.tail.tail;
+        return removed;
+    }
+
     @Override
     public String toString() {
         return element + (tail != null ? " -> " + tail : "");
@@ -37,12 +79,27 @@ public class MyLinkedList {
 
 
     public static void main(String[] args) {
+        System.out.println("Testing MyLinkedList:");
+        System.out.println("Making a list with 3 elements, num1, num2, num3");
+        MyLinkedList list1 = new MyLinkedList("num1");
+        list1.add("num2");
+        list1.add("num3");
+        System.out.println("inserting elements at idx 0 and 2");
+        list1.insert("obj_inserted_at_0", 0);
+        list1.insert("obj_inserted_at_2", 2);
+        System.out.println(list1);
+        System.out.print("list length: ");
+        System.out.println(list1.length());
+        System.out.println("Making another list with elements 'w' and 'f' and testing toString method");
         MyLinkedList myList = new MyLinkedList("w", new MyLinkedList("f"));
         System.out.println(myList.toString());
-        System.out.println(myList.length());
-        System.out.println("Add element to list");
+        System.out.println("Adding an int to list with strings...");
         myList.add(5);
+        System.out.println("Testing toString and length() on list 2");
         System.out.println(myList.toString());
         System.out.println(myList.length());
+        System.out.println("Testing delete on idx 0");
+        myList.delete(0);
+        System.out.println(myList);
     }
 }
